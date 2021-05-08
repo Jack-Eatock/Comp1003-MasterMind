8095d289142859c98fd5ec766edf3c53bc6ef995
Jack Eatock <46260379+Jack-Eatock@users.noreply.github.com>
2021-05-08 21:22:47 +0100

    Final Neaten

--- Comp1003-MasterMind/Program.cs
+++ Comp1003-MasterMind/Program.cs
@@ -2,65 +2,80 @@
 
 namespace Comp1003_MasterMind {
 
+    /// <summary>
+    /// Data structure that contains all its required functionality. 
+    /// Simply create an object of type CustomQueue. Values are added to the top / back
+    /// and removed from the bottom / front. if the queue is full, it dynamically removes from the bottom, to make space.
+    /// The size is set on creation.
+    /// </summary>
     class CustomQueue {
 
         public int QueueLength;
         public int QPos;
-        public int[] Data;
+        public int[] QData;
 
         /// <summary>
-        ///
+        /// Constructor, ensures that all variables are set appropraitely when the object is created. ( Sets queue size etc )
         /// </summary>
-        public CustomQueue(int QLength) { Data = new int[QLength]; QueueLength = QLength; QPos = -1; }
+        public CustomQueue(int QLength) { QData = new int[QLength]; QueueLength = QLength; QPos = -1; }
 
         /// <summary>
-        ///
+        /// Takes in a value and tries to add it to the top of the Queue. If the queue is full however,
+        /// the value at the bottom of the queue is removed first, making the queue dynamic.
         /// </summary>
         public void Add(int val) {
-            if (QPos == QueueLength - 1) { } // END OF QUEUE  }
-          
-            else { QPos++;  Data[QPos] = val;  }  // Increment by 1 and set the next value. 
+            // First check if the queue is already full. If it is remove one from the bottom before adding to the top.
+            if (QPos == QueueLength - 1) {Remove();}   // END OF QUEUE  
+
+            // Simply add to the end of the queue.
+            QPos++;  QData[QPos] = val;   // Increment by 1 and set the next value. 
         }
 
-        /*
+        /// <summary>
+        /// Used if a value is needed to be removed from the queue. All values are moved down the queue by one,
+        /// this naturally removes the value at the bottom / front. Creating roomn for more values to be added to the top.
+        /// </summary>
         public void Remove() {
-            if (QPos <= -1) { } // At the begining of q, cant delete.   }
+            if (QPos <= -1) { return; } // At the begining of q, cant delete.  Simply end func.
 
-            else {
-                // Shuffle - Moving everything back one, Also removing the first item in the queue.
-                // Iterate through each value in the queue soo far. Shifting each back one.
-                for (int i = 0; i < QPos; i++) {
-                    Data[i] = Data[i + 1]; // Shifts back 1.
-                }
-            }
-        }*/
+            // Shuffle - Moving everything back one, Also removing the first item in the queue.
+            // Iterate through each value in the queue soo far. Shifting each back one.
+            for (int i = 0; i < QPos; i++) {QData[i] = QData[i + 1];    } // Shifts back 1.
+            QPos--;
+        }
         
+        /// <summary>
+        /// Simple function used to return all the integer values within the queue in a single string. ( In order, from bottom to top )  
+        /// </summary>
         public string Output() {
             string output = "";
 
             // Loop up to the current queue length and add each element to the string 
-            for (int i = 0; i <= QPos; i++) { output += Data[i]; }
+            for (int i = 0; i <= QPos; i++) { output += QData[i]; }
             return (output);
         }
     }
-
+    
     class Program {
 
-        private bool isDebugging = true; // This is for testing, shows code.
-        private bool _isPlaying = true; // If true the game loop will run, otherwise the program ends.
+        private bool _isDebugging = false; // This is for testing, shows code at the start.
+        private bool _isPlaying = true;   // If true the game loop will run, otherwise the program ends.
 
-        private int _numColours = 8; // Num Colours -> M
-        private int _numPositions = 6; // Num Positions -> N [ DO BEFORE SUBMIT ]
+        private int _numColours = 8;     // Num Colours  -> (THIS IS M)
+        private int _numPositions = 6;  // Num Positions -> (THIS IS N) 
 
-        private CustomQueue _history;
-        private int[] _code;
-        private int[] _playerGuess;
+        private CustomQueue _history; // Data stucture used to store the history of gueses in order.
+        private int[] _code;         // Array of integers used to store the randomised code.
+        private int[] _playerGuess; // Array of integers  used to hold the current user guess, for comparison with the actual code.
 
-        private int _numBlack = 0;
-        private int _numWhite = 0;
+        private int _historyQSize = 10; // This is the size of the queue, if the queue goes beyond, it will start to remove values from the bottom of the queue. ( Dynamic )
+
+        private int _numBlack = 0; // The guesses that are both in the correct position and colour.
+        private int _numWhite = 0; // Wrong position but the actual code does contain that colour.
 
         /// <summary>
-        ///
+        /// This is the first function to be executed. It simply creates an object of type program, that contains all the functionality required,
+        /// Then welcomes the user, initialises the progam and runs a loop of functions that make the game actually play. If the loops ends, the program ends.
         /// </summary>
         static void Main(string[] args) {
             Program program = new Program();
@@ -79,49 +94,52 @@ namespace Comp1003_MasterMind {
         }
 
         /// <summary>
-        ///
+        /// This function prompts the user to input the values for the number of colours and positions. It then verifies the inputs are valid,
+        /// sets up all the data structures that are required through out the program, with their appropriate sizes, and generates a random code 
+        /// to the user's request. Finally if debugging it will print out the actuall code. 
         /// </summary>
         private void Intro() {
 
-            Console.WriteLine("Please enter the number of colours:");
-            bool success = int.TryParse(Console.ReadLine(), out _numColours);
-            if (!success) { Console.WriteLine("Input Invalid"); Intro(); }
+            Console.WriteLine("Please enter the number of colours (1-9) :");
+            bool isIntFlag = int.TryParse(Console.ReadLine(), out _numColours);
+            if (!isIntFlag || _numColours > 9 || _numColours <= 0) { Console.WriteLine("Input Invalid"); Intro(); }
 
             Console.WriteLine("Please enter the number of positions:");
-            success = int.TryParse(Console.ReadLine(), out _numPositions);
-            if (!success) { Console.WriteLine("Input Invalid"); Intro(); }
+            isIntFlag = int.TryParse(Console.ReadLine(), out _numPositions);
+            if (!isIntFlag) { Console.WriteLine("Input Invalid"); Intro(); }
 
-            //
-            // CHANGE MAKE OWN CLASS
             Random rnd = new Random(); int rndValu;
 
-            // Create a queue to storet the history. The size is ...
-            _history = new CustomQueue(100);
-            // CHANGE MAKE OWN CLASS
+            _history = new CustomQueue(_historyQSize);
 
             _code = new int[_numPositions]; _playerGuess = new int[_numPositions];
 
             for (int i = 0; i < _numPositions; i++) { rndValu = rnd.Next(0, _numColours); _code[i] = rndValu; }
             Console.WriteLine();
 
-            if (!isDebugging) { return; }
+            if (!_isDebugging) { return; }
 
             // This only occurs if debugging.
-            Console.Write("The code is : ");
+            Console.Write("[DEBUGGING ON] The code is : ");
             for (int i = 0; i < _numPositions; i++) { Console.Write(_code[i]); }
             Console.WriteLine();
         }
 
         /// <summary>
-        ///
+        /// This function loops for every value in the code, allows the user to enter a guess. Validates their guess is appropriate, if so adds the guess
+        /// to the guess array to be checked later.
         /// </summary>
         private void MakeAGuess() {
-
+            Console.WriteLine("Please now enter your code, each entry must be an integer between 0 and " + _numColours);
             for (int codePos = 0; codePos < _numPositions; codePos++) {
-                bool flag = false;
+                bool flag = false; bool isInt;
                 while (!flag) {
-                    Console.WriteLine("Code, Pos [ " + codePos + " ] :");
-                    flag = int.TryParse(Console.ReadLine(), out _playerGuess[codePos]);
+                    Console.WriteLine("[ENTERING YOUR CODE] - Pos [ " + codePos + " ]:");
+                    isInt = int.TryParse(Console.ReadLine(), out _playerGuess[codePos]);
+
+                    // Makes sure that the values for the code entered are within the colours available. For example if the colours are 5, they cant enter 6 as their guess.
+                    if(isInt && _playerGuess[codePos] <= _numColours && _playerGuess[codePos] > 0) { flag = true; }
+
                     if (!flag) { Console.WriteLine("Input Invalid"); }
                 }
             }
@@ -132,7 +150,8 @@ namespace Comp1003_MasterMind {
         }
 
         /// <summary>
-        ///
+        /// Iterates through the values in the code and guesscode arrays, comparing the two and calculating how many black and white pins were recieved
+        /// from this guess.
         /// </summary>
         private void CheckGuess() {
 
@@ -142,7 +161,7 @@ namespace Comp1003_MasterMind {
                 // Same pos and colour (num)
                 if (_code[i] == _playerGuess[i]) { _numBlack++; }
 
-                // Not same pos but the contains colour colour
+                // Not same pos but the code contains the same colour 
                 else if (myContains(_code, _playerGuess[i], _numPositions)) { _numWhite++; }
 
                 // Not in code 
@@ -151,12 +170,13 @@ namespace Comp1003_MasterMind {
         }
 
         /// <summary>
-        ///
+        /// Simply iterates through the player's guess and adds them to a queue data structure called _history.
         /// </summary>
         public void AddHistory() { for (int i = 0; i < _numPositions; i++) { _history.Add(_playerGuess[i]); } }
 
         /// <summary>
-        ///
+        /// Simply prints the history, that is stored within a queue with spaces inbetween each guess. 
+        /// If the queue is empty, it will instead output a message stating that the history is null. 
         /// </summary>
         private void PrintHistory() {
             string history = _history.Output();
